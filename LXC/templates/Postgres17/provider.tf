ff6b741ca72be1003345e91a73bac5f7d822d18e
Andrew Arasmith <43914177+aarasmith@users.noreply.github.com>
2025-09-08 15:34:02 +0100

    remove requirement for api token/secret

--- LXC/templates/Postgres17/provider.tf
+++ LXC/templates/Postgres17/provider.tf
@@ -11,15 +11,15 @@ variable "proxmox_api_url" {
         type = string
 }
 
-variable "proxmox_api_token_id" {
-        type = string
-        sensitive = true
-}
+# variable "proxmox_api_token_id" {
+#         type = string
+#         sensitive = true
+# }
 
-variable "proxmox_api_token_secret" {
-        type = string
-        sensitive = true
-}
+# variable "proxmox_api_token_secret" {
+#         type = string
+#         sensitive = true
+# }
 
 variable "proxmox_user" {
         type = string
