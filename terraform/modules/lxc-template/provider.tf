a09361803c85b1bdb6fff1449574389d2dd487fc
Andrew Arasmith <43914177+aarasmith@users.noreply.github.com>
2025-09-10 15:36:36 +0100

    fixing naming of provider vars again

--- terraform/modules/lxc-template/provider.tf
+++ terraform/modules/lxc-template/provider.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = var.proxmox_api_url
+  pm_api_url          = var.pm_api_url
 #   pm_user           = var.proxmox_user
 #   pm_password       = var.proxmox_password
-  pm_api_token_id     = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
   pm_tls_insecure = true
 }
 
